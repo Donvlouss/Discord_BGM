4f83c561476b8105b6d83ac8e96c52e4be56f477
Donvlouss <donvlous0rixk@gmail.com>
2021-10-04 17:12:42 +0800

    add_function

--- bgm_bot.py
+++ bgm_bot.py
@@ -1,11 +1,12 @@
-import discord
-from discord.ext import commands
-import time
-import asyncio
-import youtube_dl
-import json
 import os
+import time
+import json
+import random
+import asyncio
+import discord
+import youtube_dl
 from datetime import datetime
+from discord.ext import commands
 
 youtube_dl.utils.bug_reports_message = lambda: ''
 
@@ -30,6 +31,7 @@ ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
 bot = commands.Bot(command_prefix='!')
 path = 'https://www.youtube.com/watch?v=q6EoRBvdVPQ'
+ffmpegss = r"C:\Users\K604_DON\Documents\ffmpeg\bin\ffmpeg.exe"
 
 class YTDLSource(discord.PCMVolumeTransformer):
     def __init__(self, source, *, data, volume=0.5):
@@ -96,6 +98,8 @@ async def add_user(ctx, id, link, time, img=""):
     }
     with open('config.json', 'w', encoding='utf-8') as f:
         json.dump(data, f)
+    user = data["clients"][str(id)]
+    await ctx.send("ID:{}\nBGM:{}\nTIME:{}\nIMG:{}".format(str(id), user['bgm'], user['time'], user['img']))
 
 @bot.command()
 async def add_subm(ctx, id, img):
@@ -104,13 +108,48 @@ async def add_subm(ctx, id, img):
     }
     with open('config.json', 'w', encoding='utf-8') as f:
         json.dump(data, f)
+    await ctx.send("Add Image success!")
+
+@bot.command()
+async def add_bgm(ctx,id,link):
+    links = data["clients"][str(id)]['bgm']
+    if isinstance(links, str):
+        links = [links]
+        print(type(links))
+    links.append(link)
+    data["clients"][str(id)]['bgm'] = links
+    with open('config.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f)
+    await ctx.send("ID:{}\nBGM:{}".format(str(id), data["clients"][str(id)]['bgm']))
+        
+@bot.command()
+async def remove_bgm(ctx, id, link):
+    if isinstance(data["clients"][str(id)]['bgm'], list):
+        if len(data["clients"][str(id)]['bgm'])!=1:
+            data["clients"][str(id)]['bgm'].remove(link)
+    elif isinstance(data["clients"][str(id)]['bgm'], str):
+        # data["clients"][str(id)]['bgm'] = ""
+        pass
+    with open('config.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f)
+    await ctx.send("{} is removed!".format(link))
+
+@bot.command()
+async def list_user(ctx, id):
+    user = data["clients"][str(id)]
+    await ctx.send("ID:{}\nBGM:{}\nTIME:{}\nIMG:{}".format(str(id), user['bgm'], user['time'], user['img']))
 
 @bot.command()
 async def update_context(ctx):
     global data
     with open('config.json', 'r', encoding='utf-8') as f:
         data = json.load(f)
-    print('>>> Updata seccusful <<<')
+    await ctx.send('>>> Updata seccusful <<<')
+
+@bot.command()
+async def shutdown_security(ctx):
+    await ctx.send('>>> Bot Shutdown <<<')
+    await ctx.bot.logout()
 
 @bot.event
 async def on_voice_state_update(member, before, after):
@@ -121,7 +160,7 @@ async def on_voice_state_update(member, before, after):
         if str(member.id) in data["clients"]:
             client = data["clients"][str(member.id)]
             await asyncio.sleep(1)
-            await reply_channel.send('[{}] <@{}> is enter [{}] voic_channel! '.format(datetime.now().strftime("%H:%M:%S"), member.id, new_channel))
+            # await reply_channel.send('[{}] <@{}> is enter [{}] voic_channel! '.format(datetime.now().strftime("%H:%M:%S"), member.id, new_channel))
             if "img" in client:
                 if client['img']:
                     await reply_channel.send(file=discord.File(client['img']))
@@ -130,9 +169,15 @@ async def on_voice_state_update(member, before, after):
             vc = await vch.connect()
 
             file = client["bgm"]
+            if isinstance(file, list):
+                file = file[
+                    random.randint(0,len(file)-1)
+                ]
+                
             if "https://www.youtube.com/" in file:
                 file = await YTDLSource.from_url(url=file)
             vc.play(discord.FFmpegPCMAudio(executable=data["FFmpeg"], source=file))
+        
             await asyncio.sleep(int(client["time"]))
             await vc.disconnect()
 
